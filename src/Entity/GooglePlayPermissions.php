4b231840685cf8e4db1d509a0b41b9273c66a920
rem42 <me@remy.ovh>
2022-11-03 21:27:42 +0100

    fix: remove movie

--- src/Entity/GooglePlayPermissions.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Scraper\ScraperGooglePlay\Entity;
-
-final class GooglePlayPermissions
-{
-    public ?string $title = null;
-    public ?string $description = null;
-}
